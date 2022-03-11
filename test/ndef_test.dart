cc1f9b8a7ffa948bbab982b3bf22ba31347bb339
CheverJohn <cheverjonathan@gmail.com>
2022-03-11 20:34:46 +0800

    docs: I rebase two commit!hhh

--- test/ndef_test.dart
+++ test/ndef_test.dart
@@ -1,7 +1,8 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
+
+Best wish
 
 import 'package:ndef/ndef.dart';
 import 'package:ndef/record/bluetooth.dart';
